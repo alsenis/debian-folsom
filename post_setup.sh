cfbdde1e0f32cb65093dabed5053f278a71eee4c
Bryan <bjime008@fiu.edu>
2013-08-21 08:38:07 -0400

    modified to auto assign floating ips

--- post_setup.sh
+++ post_setup.sh
@@ -46,14 +46,19 @@ sed -i "s/^#network_manager=nova.network.manager.VlanManager/network_manager=nov
 sed -i "s/^#force_dhcp_release/force_dhcp_release/g" /etc/nova/nova.conf
 sed -i "s/^#dhcpbridge_flagfile/dhcpbridge_flagfile/g" /etc/nova/nova.conf
 sed -i "s/^#dhcpbridge/dhcpbridge/g" /etc/nova/nova.conf
+sed -i "187iauto_assign_floating_ip=True" /etc/nova/nova.conf
+
 
 ######################
 # nova-compute.conf
 #####################
 sed -i "s/^libvirt_vif_driver=.*/libvirt_vif_driver=nova.virt.libvirt.vif.LibvirtBridgeDriver/g" /etc/nova/nova-compute.conf
 
-# Create the network
+# Create the fixed (private) network
 nova-manage network create private --fixed_range_v4=10.0.0.0/25 --num_networks=1 --bridge=br100 --bridge_interface=dummy0 --network_size=128
+
+# Create the floating (public) network
+nova-manage floating create --ip_range=172.23.12.0/24
 
 # Restart services
 ./stackswitch.sh restart
