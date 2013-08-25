bb037158646c6a02d8b9f08bbe915849d4f0e480
Bryan <bjime008@fiu.edu>
2013-08-25 00:33:26 -0400

    cleaned up comments

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@
 apt-get install -y dbconfig-common
 apt-get install -y chkconfig
 apt-get install -y mysql-server
-apt-get install -y --force-yes keystone 
-#apt-get install -y --force-yes python-cinderclient python-glanceclient memcached rabbitmq-server ntp cinder-api cinder-scheduler glance nova-api nova-console nova-scheduler nova-xvpvncproxy openstack-dashboard-apache nova-api nova-cert nova-compute nova-compute-kvm nova-network cinder-volume iscsitarget
+apt-get install -y --force-yes keystone
 apt-get install -y --force-yes kvm python-cinderclient python-glanceclient memcached rabbitmq-server ntp cinder-api cinder-scheduler glance nova-api nova-console nova-scheduler nova-xvpvncproxy openstack-dashboard-apache nova-api nova-cert nova-compute nova-compute-kvm nova-network cinder-volume cinder-scheduler tgt
